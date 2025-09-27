631daafcc5a94c89509f889961be06576820aa67
ShreyasSomannaIU <shreyassomanna46@gmail.com>
2025-09-27 11:59:48 +0530

    added gitIgnore

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -9,13 +9,11 @@ app.use(cors({
     credentials: true
 })) // Enable CORS for all routes
 
+
 app.use(express.json({limit: "16kb"})) // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true, limit: "20kb" })) // Middleware to parse URL-encoded bodies
 app.use(express.static("public")) // Serve static files from the "public" directory
 app.use(cookieParser()) // Middleware to parse cookies
-
-
-
 
 
 export default app
